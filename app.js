e3b12a6d651eb9e94aa923ffcc6e7e2b4ffbe6a5
lucas5252git <lucaselijah6@gmail.com>
2024-05-27 18:23:16 +0100

    new one

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8080;
 app.use(express.static('public'));
 
-// app.get('/', (req, res) => res.send(' THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED WITH AWS. THIS PAGE INDICATES SUCCESS. YOURNAME: LUCAS ELIJAH LONGKET MATRIC NUMBER: VUG/CSC/21/5252'));
+app.get('/', (req, res) => res.send(' THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED WITH AWS. THIS PAGE INDICATES SUCCESS. YOURNAME: LUCAS ELIJAH LONGKET MATRIC NUMBER: VUG/CSC/21/5252'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
